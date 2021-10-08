3381704e7d6522eb9d0868cfc75d593272e90c47
goyoun <qqqeee1234@naver.com>
2021-10-08 17:19:48 +0900

    오늘배운 주사위

---
+++ src/git01/Ex02_whileEx.java
@@ -0,0 +1,23 @@
+package git01;
+
+public class Ex02_whileEx {
+
+	public static void main(String[] args) {
+		//
+		
+	boolean run = true;
+		
+		while (run) {
+			int i = (int) (Math.random() * 6) + 1;
+			int j = (int) (Math.random() * 6) + 1;
+			System.out.println("(" + i + ", " + j + ")");
+		
+			if ( ( i + j ) == 5 ){
+				System.out.println("주사위의합이 " + (i+j) + " 이므로 종료되었습니다.");
+				run = false;
+			}
+		}
+
+	}
+
+}
